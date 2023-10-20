0a634f7dbb2fb0a1552040e0cb956e446ae48ec2
Picred <andry4you@hotmail.it>
2023-10-20 09:13:54 +0200

    gpu: update

--- gpu/opencl/vecinit.c
+++ gpu/opencl/vecinit.c
@@ -8,7 +8,7 @@ void vecinit_k(int i, int *array){ //parte device
     array[i] = i;
 }
 
-cl_event vecinit(cl_command_que que, cl_kernel vecinit_k, int nels, cl_mem array){
+cl_event vecinit(cl_command_queue que, cl_kernel vecinit_k, int nels, cl_mem d_array){
 	cl_event vecinit_evt;
     cl_int err;
     err = clSetKernelArg(vecinit_k, 0, sizeof(d_array), &d_array);
@@ -16,7 +16,7 @@ cl_event vecinit(cl_command_que que, cl_kernel vecinit_k, int nels, cl_mem array
 
     // chiamare kernelvecinit nel volte su *array
     const size_t gws[] = {nels}; //dimensione work group, global work size
-    cl_int err = clEnqueueNDRangeKernel(que, vecinit_k, 1, NULL, gws, NULL, 0, NULL, &vecinit_k);
+    err = clEnqueueNDRangeKernel(que, vecinit_k, 1, NULL, gws, NULL, 0, NULL, &vecinit_k);
     // i parametri "1, NULL, gws, NULL " specificano la griglia di lancio
     // err rappresenta l'eventuale errore dell'accodamento del kernel
     ocl_check(err, "vecinit kernel enqueue failed\n");
@@ -69,7 +69,7 @@ int main(int argc, char*argv[]){
     cl_context ctx = create_context(p,d);
     cl_command_queue que = create_queue(ctx,d);
     // compilare la parte device del programma
-    cl_program prog = create_program("vecinit.ocl", ctl, d); //codice device + contesto + device d
+    cl_program prog = create_program("vecinit.ocl", ctx, d); //codice device + contesto + device d
 
     // TODO: allocare la RAM su device (d_array), array su device
     // non esiste l'equivalente di un malloc(). Si crea un buffer nel ctx e si può spostare da device a device 
@@ -89,10 +89,10 @@ int main(int argc, char*argv[]){
     // TODO: lanciare il kernel vecinit su d_array
     cl_event vecinit_evt = vecinit(que, vecinit_k,nels,h_array);
 
-    cl_event wait_list[] { vecinit_evt}; //se avessimo piu eventi da aspettare e andrebbe sostituito in *vecinit_evt nella fuznione
+    cl_event wait_list[] = { vecinit_evt}; //se avessimo piu eventi da aspettare e andrebbe sostituito in *vecinit_evt nella fuznione
     cl_event read_evt;
     // TODO: serve, alla fine, copiare i dati da d_array a h_array
-    err =  clEnqueueReadBuffer(que,d_array, CL_TRUE, 0, memsize, h_array, 1, *vecinit_evt, &read_evt); //da buffer opencl a un puntatore host. Legge contenuti buffer in un array allocato in how. err è un errore sull'accodamento. se il comando va bene, allora l'accodamento è andato bene ma non vuol dire che la copia sia effettivamente avvenuta.
+    err =  clEnqueueReadBuffer(que,d_array, CL_TRUE, 0, memsize, h_array, 1, &vecinit_evt, &read_evt); //da buffer opencl a un puntatore host. Legge contenuti buffer in un array allocato in how. err è un errore sull'accodamento. se il comando va bene, allora l'accodamento è andato bene ma non vuol dire che la copia sia effettivamente avvenuta.
     // la funzione non ritorna finche copia non è finita visto che ho CL_TRUE ed è bloccante. Alla fine della funzione gli elementi sono copiati su h_array.
     // se avessi invece CL_FALSE, allora si tratta di copia asincrona, quindi posso fare altre cose che non coinvolgono h_array, e prima di usare h_array dovrei sincronizzarmi. ma si vedrà dopo
     // clEnqueueWriteBuffer //scrive su un buffer da un puntatore a host, quindi da host.
@@ -106,7 +106,7 @@ int main(int argc, char*argv[]){
 
     int ret = verify(nels,h_array);
     
-    double vecinict_ms = runtime_ms(vecinit_evt);
+    double vecinit_ms = runtime_ms(vecinit_evt);
     double read_ms = runtime_ms(read_evt);
     printf("init: %.2g ms\n", vecinit_ms);
     printf("read: %.2g ms\n", read_ms);
