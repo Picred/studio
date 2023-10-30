b26bdb02dbbca2fd8d4420c7bb224f593d93f348
Andrei <102211437+Picred@users.noreply.github.com>
2023-10-30 15:35:42 +0000

    contest: greedy-activity

---
+++ Contest/greedy-activity.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Da verificare con input di CContest
+
+class Activity{
+    private:
+        int s;
+        int f;
+    public:
+        Activity(int _s, int _f) : s(_s), f(_f){}
+    
+        int getS(){return s;}
+        
+        int getF(){return f;}        
+};
+
+void actSort(Activity** acts, int n){
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(acts[i]->getF() >= acts[j]->getF())
+                swap(acts[i], acts[j]);
+        }   
+    }
+}
+
+void solve(int n, ifstream &in, ofstream&out){
+    char tmp;
+    Activity** acts = new Activity*[n];
+
+    for(int i=0; i<n; i++){
+        int s;
+        int f;
+        in >> tmp >> s >> tmp >> f >> tmp;
+        acts[i] = new Activity(s,f);
+    }
+
+    actSort(acts,n);
+
+    int lastSelected=0;
+    int counter = 1;
+    
+    for(int i=1; i<n; i++){
+        if(acts[i]->getS() >= acts[lastSelected]->getF()){
+            lastSelected = i;
+            counter++;
+        }
+    }
+
+    out << counter << endl;
+}
+
+
+int main(){
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    int n;
+
+    for(int i=0; i<100; i++){
+        in >> n;
+        solve(n,in,out);
+    }
+}
