db21f35930280976fd39def364f41604768b7d1d
Andrei <102211437+Picred@users.noreply.github.com>
2023-11-03 09:47:09 +0000

    tsdw: thread learn

---
+++ TsdwLab/Thread/tests/Shared.java
@@ -0,0 +1,29 @@
+public class Shared{
+    private int number = 0;
+    private int turn;
+
+    public void add(){
+        this.number++;
+        this.turn = 0;
+    }
+
+    public int getN(){
+        return this.number;
+    }
+
+    public int getTurn(){
+        return this.turn;
+    }
+
+    public synchronized void sharedWait() throws InterruptedException{
+        wait();
+    }
+
+    public synchronized void sharedNotify(){
+        notifyAll();
+    }
+
+    public void setTurn(int t){
+        this.turn = t;
+    }
+}
