3fc8b91fca4da4c931b885cdbb00915395543ed7
Picred <andry4you@hotmail.it>
2023-11-09 16:36:02 +0100

    tsdw: itinere

---
+++ TsdwLab/Esercizi/Itinere/client.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]){
+    int sockfd, n;
+    socklen_t len = sizeof(struct sockaddr_in);
+    struct sockaddr_in remote_addr;
+    char buffer[BUFSIZ];
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("socket");
+        return -1;
+    }
+    if((connect(sockfd, (struct sockaddr*)&remote_addr, len)) < 0){
+        perror("connect");
+        return -1;
+    }
+
+    memset(&remote_addr, 0, len);
+    remote_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(3333);
+
+    strcpy(buffer, "test");
+
+    if((send(sockfd, buffer, strlen(buffer), 0)) < 0){
+        perror("send");
+        return -1;
+    }
+
+    if((n = recv(sockfd, buffer, BUFSIZ, 0)) < 0){
+        perror("recv");
+        return -1;
+    }
+
+    buffer[n] = 0;
+
+    printf("Received: %s" , buffer);
+
+    return 0;
+}
