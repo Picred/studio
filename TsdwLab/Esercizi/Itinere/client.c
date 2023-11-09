1b99a48dbb0a225bff565affd099a4ee9668cb8c
Andrei <102211437+Picred@users.noreply.github.com>
2023-11-09 15:57:09 +0000

    tsdw: completata itinere

--- TsdwLab/Esercizi/Itinere/client.c
+++ TsdwLab/Esercizi/Itinere/client.c
@@ -3,6 +3,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+// $ ./client 0.0.0.0 $'pippo\n'
 
 int main(int argc, char* argv[]){
     int sockfd, n;
@@ -14,17 +15,18 @@ int main(int argc, char* argv[]){
         perror("socket");
         return -1;
     }
+
+    memset(&remote_addr, 0, len);
+    remote_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(3333);
+    
     if((connect(sockfd, (struct sockaddr*)&remote_addr, len)) < 0){
         perror("connect");
         return -1;
     }
 
-    memset(&remote_addr, 0, len);
-    remote_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    remote_addr.sin_family = AF_INET;
-    remote_addr.sin_port = htons(3333);
-
-    strcpy(buffer, "test");
+    strcpy(buffer, argv[2]);
 
     if((send(sockfd, buffer, strlen(buffer), 0)) < 0){
         perror("send");
