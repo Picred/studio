f33b784a69fe104968f0b948f93a3fc33f7d7d7b
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2023-12-04 09:50:31 +0000

    contest: matrix chain

--- Contest/matrix-chain.cpp
+++ Contest/matrix-chain.cpp
@@ -4,31 +4,10 @@
 
 using namespace std;
 
-int matrixchain(int* p, int n){
-    int m[n][n];
-
-    for(int i=1; i<n; i++){
-        m[i][i] = 0;
-    }
-
-    for(int l=2; l<n; l++){
-        for(int i=1; i<n-l+1; i++){
-            int j = i+l-1;
-            m[i][j] = INT_MAX;
-            for(int k=i; k<j; k++){
-                int q = m[i][k] + m[k+1][j] + (p[i-1] * p[k] * p[j]);
-                if (q < m[i][j])
-                    m[i][j] = q;
-            }
-        }
-    }
-    return m[1][n-1];
-}
-
-int solve(ifstream &in, int n){
+void solve(ifstream& in, ofstream& out, int n){
+    int *p = new int[n+1]{0};
     char tmp;
     int dim1, dim2;
-    int* p = new int[n+1];
 
     for(int i=0; i<n; i++){
         in >> tmp >> dim1 >> tmp >> dim2 >> tmp;
@@ -36,16 +15,32 @@ int solve(ifstream &in, int n){
     }
     p[n] = dim2;
 
-    return matrixchain(p,n+1);
+    n+=1;
+    int m[n][n];
+
+    for(int i=1; i<n; i++)
+        m[i][i] = 0;
+    
+    for(int x=2; x<n; x++)
+        for(int i=1; i<n-x+1; i++){
+            int j = i+x-1;
+            m[i][j] = INT_MAX;
+            for(int k = i; k<j; k++){
+                int q = m[i][k] + m[k+1][j] + (p[i-1] * p[k] * p[j]);
+                if(q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+
+        out << m[1][n-1] << endl;
 }
 
 int main(){
     ifstream in("input.txt");
     ofstream out("output.txt");
-
     int n;
     for(int i=0; i<100; i++){
         in >> n;
-        out << solve(in,n) << endl;
+        solve(in,out,n);
     }
 }
