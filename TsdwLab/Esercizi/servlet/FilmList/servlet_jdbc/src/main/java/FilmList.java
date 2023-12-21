8c155d96d0e553b8003e7c6ea81d46790a774be5
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2023-12-21 17:30:11 +0000

    tsdw: FilmList Servlet

--- TsdwLab/Esercizi/servlet/FilmList/servlet_jdbc/src/main/java/FilmList.java
+++ TsdwLab/Esercizi/servlet/FilmList/servlet_jdbc/src/main/java/FilmList.java
@@ -1,113 +1,164 @@
 import java.sql.*;
+
+import javax.xml.transform.Result;
+
 import jakarta.servlet.annotation.WebServlet;
-import java.io.*;
 import jakarta.servlet.http.*;
+import java.io.*;
 
 @WebServlet("/servlet")
 public class FilmList extends HttpServlet{
     Connection conn;
-    String connString = "jdbc:mysql://localhost:3306/myDB?user=user&password=password";
+    final String connString = "jdbc:mysql://localhost:3306/myDB?user=user&password=password";
     
     
     public void init(){
         try{
             conn = DriverManager.getConnection(connString);
-            System.out.println("Connessione effettuata");
         }catch(Exception e){
-            System.out.println("Connessione fallita");
             e.printStackTrace();
         }
     }
 
-
     public void doGet(HttpServletRequest request, HttpServletResponse response){
         response.setContentType("text/html");
         PrintWriter out;
-
         try{
             out = response.getWriter();
-            out.write("<h1>Benvenuto</h1>");
-            out.write("<h2>Film consigliato: </h1>");
 
+            out.print("<h1>Film consigliato:</h1>");
+            //get random film from flist
             String sql = "SELECT * FROM flist ORDER BY RAND() LIMIT 1";
             ResultSet res = conn.createStatement().executeQuery(sql);
 
             while(res.next()){
                 String titolo = res.getString("titolo");
                 String regista = res.getString("regista");
-
-                out.write("<b>Titolo: </b>" + titolo);
-                out.write("<b>Regista: </b>" + regista + "<br>");
+                out.print("<b> titolo: </b>" + titolo);
+                out.print(" <b> regista: </b>" + regista);
+                out.print("<br>");
             }
 
-            //Fine film consigliato
-            // Cerco titolo e regista che dice user
+            // form inserimento titolo e regista per la ricerca in flist
+            out.print("<h2>Ricerca film<h2>");
+            out.print("<form action='/servlet' method='POST'>");
+            out.print("<input type='text' name='titolo' placeholder='Titolo'>");
+            out.print("<input type='text' name='regista' placeholder='Regista'>");
+            out.print("<input type='hidden' name='action' value='cerca'>");
+            out.print("<input type='submit' value='Cerca in flist'>");
+            out.print("</form>");
 
-            out.write("<form action='/servlet' method='POST'>");
-            out.write("<input type='hidden' name='action' value='search'>");
-            out.write("<input type='text' name='titolo' placeholder='titolo' required>");
-            out.write("<input type='text' name='regista' placeholder='regista' required>");
-            out.write("<input type='submit' value='Cerca'>");
-            out.write("</form>");
+            //form vedi wlist
+            out.print("<h2>Visione wish list <code>wlist</code><h2>");
+            out.print("<form action='/servlet' method='POST'>");
+            out.print("<input type='hidden' name='action' value='seewlist'>");
+            out.print("<input type='submit' value='Vedi wlist'>");
+            out.print("</form>");
 
-        }catch (Exception e){
+            //form svuota wlist
+            out.print("<h2>Svuota wish list <code>wlist</code><h2>");
+            out.print("<form action='/servlet' method='POST'>");
+            out.print("<input type='hidden' name='action' value='deletewlist'>");
+            out.print("<input type='submit' value='Svuota wlist'>");
+            out.print("</form>");
+
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
-
-
 
     public void doPost(HttpServletRequest request, HttpServletResponse response){
         response.setContentType("text/html");
         PrintWriter out;
         try{
             out = response.getWriter();
-            if(request.getParameter("action").equals("search")){
-                // cerca dati indicati dall'utente
+
+            //search in flist
+            if(request.getParameter("action").equals("cerca")){
+                // cerco in flist titolo e regista dati da request dal form di ricerca
                 String titolo = request.getParameter("titolo");
                 String regista = request.getParameter("regista");
+                String sql = "SELECT * FROM flist WHERE titolo='"+titolo+"' AND regista='" + regista +"'";
 
-                String sql = "SELECT * FROM flist WHERE titolo='" + titolo + "' AND regista='" + regista + "'";
                 ResultSet res = conn.createStatement().executeQuery(sql);
 
-                while(res.next()){
-                    out.write("Titolo: " + res.getString("titolo") + " ");
-                    out.write("Regista: " + res.getString("regista") + "<br>");
+                if(!res.next()){
+                    out.print("Nessun risultato trovato, vuoi aggiungerlo alla <code>wlist</code>?");
+                    //form per aggiungere alla wlist si o no
+                    out.print("<form action='/servlet' method='POST'>");
+                    out.print("<input type='hidden' name='titolo' value='"+ titolo +"'>");
+                    out.print("<input type='hidden' name='regista' value='" + regista + "'>");
+                    out.print("<input type='hidden' name='action' value='addwlist'>");
+                    out.print("<input type='submit' name='si' value='Si'>");
+                    out.print("<input type='submit' name='no' value='No'>");
+                    out.print("</form>");
+                }else {
+                    out.print("<h1>Film richiesto: </h1> <br>");
+                    do{
+                        String titoloTrovato = res.getString("titolo");
+                        String registaTrovato = res.getString("regista");
+                        out.print("<b> titolo: </b>" + titoloTrovato);
+                        out.print(" <b> regista: </b>" + registaTrovato);
+                        out.print("<br>");
+                    }while(res.next());
                 }
-                // out.write("Risultato non trovato in tabella, vuoi aggiungerlo alla <code>wlist</code>?<br>");
-                // out.write("<form action='/servlet' method='POST'>");
-                // out.write("<input type='hidden' name='action' value='wlist'>");
-                // out.write("<input type='hidden' name='titolo' value='"+ titolo + "'>");
-                // out.write("<input type='hidden' name='regista' value='"+ regista + "'>");
-                // out.write("<input type='submit' name='si' value='si'>");
-                // out.write("<input type='submit' name='no' value='no'>");
-                // out.write("</form>");
                 homepage(out);
             }
-
-            if(request.getParameter("action").equals("wlist")){
-                if(request.getParameter("si") != null){
-                    // add in wlist
+            
+            //Insert in wlist
+            else if(request.getParameter("action").equals("addwlist")){
+                if(request.getParameter("si") != null && request.getParameter("si").equals("Si")){
                     String titolo = request.getParameter("titolo");
                     String regista = request.getParameter("regista");
-                    String sql = "INSERT INTO wlist (titolo, regista) VALUES (?,?)";
+                    String sql = "INSERT INTO wlist (titolo, regista) VALUES (?, ?)";
                     PreparedStatement st = conn.prepareStatement(sql);
                     st.setString(1, titolo);
                     st.setString(2, regista);
                     st.executeUpdate();
+                    out.print("Aggiunto in wlist");
                 }
-                else if(request.getParameter("no") != null){
-                    out.write("Inserimento annullato in <code>wlist</code>");
-                    homepage(out);
+                else{
+                    out.print("Film non aggiunto in wlist.");
                 }
+                homepage(out);
             }
 
-        }catch (Exception e){
+            //vedi tutto wlist
+            else if(request.getParameter("action").equals("seewlist")){
+                String sql = "SELECT * FROM wlist";
+                ResultSet res = conn.createStatement().executeQuery(sql);
+
+                if(!res.next()){
+                    out.print("<code>wlist</code> vuota");
+                }
+                else{
+                    out.print("<h1>Wish list:</h1><br>");
+                    do{
+                        String titolo = res.getString("titolo");
+                        String regista = res.getString("regista");
+                        out.print("<b> titolo: </b>" + titolo);
+                        out.print(" <b> regista: </b>" + regista);
+                        out.print("<br>");
+                    }while(res.next());
+                }
+                homepage(out);
+            }
+
+            //svuota wlist
+            else if(request.getParameter("action").equals("deletewlist")){
+                String sql = "DELETE FROM wlist";
+                conn.createStatement().execute(sql);
+                out.print("<code>wlist svuotata");
+                homepage(out);
+            }
+
+
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
 
     public void homepage(PrintWriter out){
-        out.write("<br><a href='/servlet'>Homepage</a>");
+        out.print("<br><a href='/servlet'>Homepage</a>");
     }
 }
